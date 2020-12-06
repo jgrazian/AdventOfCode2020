7ad3b13b8fd87c93fe714e1082804debd6f316dd
Joe Graziano <joeyg11@vt.edu>
2020-12-06 10:52:27 -0500

    Day 7

---
+++ src/day06/day06_b.ts
@@ -0,0 +1,31 @@
+import { isPosixPathSeparator } from "https://deno.land/std@0.79.0/path/_util.ts";
+import { getInput, tick, tock } from '../common.ts';
+
+const input = await getInput(6);
+const forms = input.split('\n');
+(forms[forms.length - 1] != '') ? forms.push('') : '';
+
+tick();
+
+const answers = new Uint16Array(26);
+let totalQuestions = 0;
+let numGroup = 0;
+for (let form of forms) {
+
+    if (form == '') {
+        totalQuestions += answers.map(v =>  Number(v == numGroup)).reduce((prev, curr) => curr + prev);
+        numGroup = 0;
+        answers.fill(0);
+        continue;
+    }
+
+    numGroup++;
+    for (let question of form) {
+        const idx = question.charCodeAt(0) - 97;
+        answers[idx]++;
+    }
+
+}
+
+console.log(tock())
+console.log(totalQuestions);
