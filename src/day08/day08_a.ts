7a7cde5626bddc09ac75304953b3076b7e9f7697
jgrazian <JOSEPH.GRAZIANO@ROCKWELLCOLLINS.COM>
2020-12-08 15:07:21 -0500

    Cleanup Day 8

--- src/day08/day08_a.ts
+++ src/day08/day08_a.ts
@@ -1,10 +1,8 @@
 import { getInput, tick, tock } from '../common.ts';
 
 const input = await getInput(8);
-const operations = input.split('\n');
 
 enum OpCode {
-    Err = -2,
     Ret = -1,
     Nop = 0,
     Acc = 1,
@@ -14,74 +12,71 @@ enum OpCode {
 interface Op {
     name: string;
     code: OpCode;
-    fn: (acc: number, ptr: number, ...ops: number[]) => [number, number];
 }
 
 const ops: Op[] = [
-    {name: 'nop', code: OpCode.Nop, fn: (acc: number, ptr: number, ...ops: number[]) => [acc, ptr + 2]},
-    {name: 'acc', code: OpCode.Acc, fn: (acc: number, ptr: number, ...ops: number[]) => [acc + ops[0], ptr + 2]},
-    {name: 'jmp', code: OpCode.Jmp, fn: (acc: number, ptr: number, ...ops: number[]) => [acc, ptr + 2*ops[0]]}
+    {name: 'nop', code: OpCode.Nop},
+    {name: 'acc', code: OpCode.Acc},
+    {name: 'jmp', code: OpCode.Jmp}
 ];
 const opNameMap: Map<string, Op> = new Map(ops.map(op => [op.name, op]));
-const opCodeMap: Map<number, Op> = new Map(ops.map(op => [op.code, op]));
 
 class Computer {
     tape: Int16Array;
-    working: Int16Array; //Copy of tape to be mutated between runs if neccisary
-    accumulator: number = 0;
+    acc: number = 0;
     ptr: number = 0;
 
     constructor(instructions: string) {
         this.tape = this.load(instructions);
-        this.working = this.tape.slice();
     }
 
-    run(): number {
-        this.accumulator = 0;
-        this.ptr = 0;
-
-        let opCode = this.working[this.ptr];
-        while (opCode != OpCode.Ret) {
-            let op = opCodeMap.get(opCode);
-            if (!op) {
-                throw `Unknown op code: ${opCode}`
-            }
-            [this.accumulator, this.ptr] = op.fn(this.accumulator, this.ptr, this.working[this.ptr + 1]);
-            opCode = this.working[this.ptr];
+    doOp() {
+        let op = this.tape[this.ptr + 0];
+        let arg = this.tape[this.ptr + 1];
+        if (op == OpCode.Nop) {
+            this.ptr += 2;
+            return true;
+        } 
+        if (op == OpCode.Acc) {
+            this.acc += arg;
+            this.ptr += 2;
+            return true;
+        } 
+        if (op == OpCode.Jmp) {
+            this.ptr += 2*arg;
+            return true;
         }
-        return this.accumulator;
+        if (op == OpCode.Ret) {
+            return false;
+        }
     }
 
-    runTo(ptrIdx: number): number {
-        this.working = this.tape.slice();
-        this.working[ptrIdx] = OpCode.Ret;
+    /// Runs tape from ptr to ptr
+    run(from?: number, to?: number): number {
+        this.acc = 0;
+        this.ptr = from || 0;
 
-        const res = this.run();
-        this.working = this.tape.slice();
-
-        return res;
+        while (this.doOp()) {
+            if (this.ptr == to) {
+                return this.acc;
+            }
+        }
+        return this.acc;
     }
 
     findLoop(): number {
-        this.accumulator = 0;
         this.ptr = 0;
-        
-        let opCode = this.working[this.ptr];
-        while (opCode != OpCode.Ret) {
-            let op = opCodeMap.get(opCode);
-            if (!op) {
-                throw `Unknown op code: ${opCode}`
-            }
-            this.working[this.ptr] = OpCode.Err; // Set visited op codes to break (-1)
-            let prevPtr = this.ptr;
-            [this.accumulator, this.ptr] = op.fn(this.accumulator, this.ptr, this.working[this.ptr + 1]);
-            opCode = this.working[this.ptr];
+        let seen = new Set();
 
-            if (opCode == OpCode.Err) {
+        let prevPtr = 0;
+        while (this.doOp()) {
+            if (seen.has(this.ptr)) {
                 return prevPtr;
             }
+            seen.add(this.ptr);
+            prevPtr = this.ptr;
         }
-        return -1; //
+        return -1;
     }
 
     load(instructions: string): Int16Array {
@@ -90,10 +85,7 @@ class Computer {
 
         let ptr = 0;
         for (let [_, op, arg] of OpVal) {
-            let opCode = opNameMap.get(op);
-            if (opCode == null) {
-                throw `Unkown op name: ${op}`;
-            }
+            let opCode = opNameMap.get(op)!;
             tape[ptr + 0] = opCode.code;
             tape[ptr + 1] = parseInt(arg);
             ptr += 2;
@@ -105,7 +97,7 @@ class Computer {
 
 tick();
 let c = new Computer(input);
-let ans = c.runTo(c.findLoop());
+let ans = c.run(0, c.findLoop());
 
 console.log(tock());
 console.log(ans);
--- src/day08/day08_b.ts
+++ src/day08/day08_b.ts
@@ -1,10 +1,8 @@
 import { getInput, tick, tock } from '../common.ts';
 
 const input = await getInput(8);
-const operations = input.split('\n');
 
 enum OpCode {
-    Err = -2,
     Ret = -1,
     Nop = 0,
     Acc = 1,
@@ -14,137 +12,108 @@ enum OpCode {
 interface Op {
     name: string;
     code: OpCode;
-    fn: (acc: number, ptr: number, ...ops: number[]) => [number, number];
 }
 
 const ops: Op[] = [
-    {name: 'nop', code: OpCode.Nop, fn: (acc: number, ptr: number, ...ops: number[]) => [acc, ptr + 2]},
-    {name: 'acc', code: OpCode.Acc, fn: (acc: number, ptr: number, ...ops: number[]) => [acc + ops[0], ptr + 2]},
-    {name: 'jmp', code: OpCode.Jmp, fn: (acc: number, ptr: number, ...ops: number[]) => [acc, ptr + 2*ops[0]]}
+    {name: 'nop', code: OpCode.Nop},
+    {name: 'acc', code: OpCode.Acc},
+    {name: 'jmp', code: OpCode.Jmp}
 ];
 const opNameMap: Map<string, Op> = new Map(ops.map(op => [op.name, op]));
-const opCodeMap: Map<number, Op> = new Map(ops.map(op => [op.code, op]));
 
 class Computer {
-    tape: Int16Array;
-    working: Int16Array; //Copy of tape to be mutated between runs if needed.
-    accumulator: number = 0;
+    tape: Int32Array;
+    acc: number = 0;
     ptr: number = 0;
 
     constructor(instructions: string) {
         this.tape = this.load(instructions);
-        this.working = this.tape.slice();
     }
 
-    /// Runs working memory
-    run(): number {
-        this.accumulator = 0;
-        this.ptr = 0;
-
-        let opCode = this.working[this.ptr];
-        while (opCode != OpCode.Ret) {
-            let op = opCodeMap.get(opCode);
-            if (!op) {
-                throw `Unknown op code: ${opCode}`
-            }
-            [this.accumulator, this.ptr] = op.fn(this.accumulator, this.ptr, this.working[this.ptr + 1]);
-            opCode = this.working[this.ptr];
+    doOp() {
+        let op = this.tape[this.ptr + 0];
+        let arg = this.tape[this.ptr + 1];
+        if (op == OpCode.Nop) {
+            this.ptr += 2;
+            return true;
+        } 
+        if (op == OpCode.Acc) {
+            this.acc += arg;
+            this.ptr += 2;
+            return true;
+        } 
+        if (op == OpCode.Jmp) {
+            this.ptr += 2*arg;
+            return true;
         }
-        return this.accumulator;
+        if (op == OpCode.Ret) {
+            return false;
+        }
     }
 
-    /// Resets working memory to tape and runs to specified ptr index
-    runTo(ptrIdx: number): number {
-        this.working = this.tape.slice();
-        this.working[ptrIdx] = OpCode.Ret;
+    /// Runs tape from ptr to ptr
+    run(from?: number, to?: number): number {
+        this.acc = 0;
+        this.ptr = from || 0;
 
-        const res = this.run();
-        this.working = this.tape.slice();
-
-        return res;
-    }
-
-    /// Checks working memory for loop, returns ptr index of op that causes loop or -1 if no loop found
-    findLoop(): number {
-        this.accumulator = 0;
-        this.ptr = 0;
-        
-        let opCode = this.working[this.ptr];
-        while (opCode != OpCode.Ret) {
-            let op = opCodeMap.get(opCode);
-            if (!op) {
-                throw `Unknown op code: ${opCode}`
+        while (this.doOp()) {
+            if (this.ptr == to) {
+                return this.acc;
             }
-            this.working[this.ptr] = OpCode.Err; // Set visited op codes to err (-2)
-            let prevPtr = this.ptr;
-            [this.accumulator, this.ptr] = op.fn(this.accumulator, this.ptr, this.working[this.ptr + 1]);
-            opCode = this.working[this.ptr];
+        }
+        return this.acc;
+    }
 
-            if (opCode == OpCode.Err) {
+    findLoop(): number {
+        this.ptr = 0;
+        let seen = new Set();
+
+        let prevPtr = 0;
+        while (this.doOp()) {
+            if (seen.has(this.ptr)) {
                 return prevPtr;
             }
+            seen.add(this.ptr);
+            prevPtr = this.ptr;
         }
-        return -1; //No err found -> no loop
+        return -1;
     }
 
     /// Fixes loop in tape memory and returns value of accumulator after fixing
     fixLoop(): number {
+        this.ptr = 0;
         const badOpPtr = this.findLoop();
 
-        this.working = this.tape.slice();
-        this.accumulator = 0;
-        this.ptr = 0;
+        let opStack:[number, OpCode][] = []; // [[ptrIdx, OpCode]]
+        while (this.doOp()) {
+            // We got to the spot where the infinite loop happens
+            if (this.ptr == badOpPtr) break;
 
-        let lastOpPtr: number[] = [];
-        let lastOpCode: OpCode[] = [];
-
-        let opCode = this.working[this.ptr];
-        while (opCode != OpCode.Ret) {
-            // We got to the spot where the infinite loop occours
-            if (this.ptr == badOpPtr) {
-                break;
+            // Hold onto nop and jmp to test later
+            let opCode = this.tape[this.ptr];
+            if (opCode == OpCode.Nop || opCode == OpCode.Jmp) {
+                opStack.push([this.ptr, opCode]);
             }
-            let op = opCodeMap.get(opCode);
-            if (!op) {
-                throw `Unknown op code: ${opCode}`
-            }
-            // Hold onto location of nop and jmp
-            if (op.code == OpCode.Nop || op.code == OpCode.Jmp) {
-                lastOpPtr.push(this.ptr);
-                lastOpCode.push(op.code);
-            }
-            [this.accumulator, this.ptr] = op.fn(this.accumulator, this.ptr, this.working[this.ptr + 1]);
-            opCode = this.working[this.ptr];
         }
 
-        // Test every ptr in backlog to see if it was the one to cause the loop
-        let isLoop = true;
-        let curOpPtr = 0;
-        let curOpCode = OpCode.Nop;
-        while (isLoop) {
-            this.working = this.tape.slice();
-            curOpPtr = lastOpPtr.pop()!;
-            curOpCode = lastOpCode.pop()!;
-            this.working[curOpPtr] = (curOpCode == OpCode.Nop) ? OpCode.Jmp : OpCode.Nop;
-            isLoop = this.findLoop() != -1;
+        // Test every nop/jmp in backlog to see if it was the one to cause the loop
+        for (let [ptr, code] of opStack) {
+            this.tape[ptr] = (code == OpCode.Nop) ? OpCode.Jmp : OpCode.Nop;
+            if (this.findLoop() == -1) {
+                return this.run();
+            }
+            this.tape[ptr] = (this.tape[ptr] == OpCode.Nop) ? OpCode.Jmp : OpCode.Nop; //Reset
         }
-        // Reset one more time and fix the loop
-        this.working = this.tape.slice();
-        this.working[curOpPtr] = (curOpCode == OpCode.Nop) ? OpCode.Jmp : OpCode.Nop;
-
-        return this.run();
+        return -1;
     }
 
-    load(instructions: string): Int16Array {
-        let tape = new Int16Array(Math.ceil(instructions.length / 3));
+    load(instructions: string): Int32Array {
+        let tape = new Int32Array(Math.ceil(instructions.length / 3));
         let OpVal = instructions.matchAll(/(^\w*) ([+-]\d*)/gm);
 
         let ptr = 0;
         for (let [_, op, arg] of OpVal) {
-            let opCode = opNameMap.get(op);
-            if (opCode == null) {
-                throw `Unkown op name: ${op}`;
-            }
+            let opCode = opNameMap.get(op)!;
             tape[ptr + 0] = opCode.code;
             tape[ptr + 1] = parseInt(arg);
             ptr += 2;
